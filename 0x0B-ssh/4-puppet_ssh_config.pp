9cd6142df91c93f2941b0c1b83715b0ff7749e2b
karim bahri <karim-bahri-18@outlook.fr>
2020-08-04 22:31:11 +0000

    without password

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # config ssh to login without password
-exec { '/etc/ssh/ssh_config' :
+exec { '/etc/ssh/ssh_config':
 path     => '/bin',
 command  => 'echo "IdentityFile ~/.ssh/holberton\nPasswordAuthentication no" >> /etc/ssh/ssh_config'
 provider => 'shell',
