66a90a5eb65eec08de36629dc5f15b8753f7debb
root <root@LAPTOP-96PPJ4QI.localdomain>
2020-09-17 19:03:01 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ def number_of_subscribers(subreddit):
     """return number of subscribers
        reddit"""
     h = {'user-agent': 'GEEK1050'}
-    link = "https://www.reddit.com/r/{}/about".format(subreddit)
+    link = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     req = requests.get(link, headers=h)
 
     return req.json().get("data").get("subscribers")
