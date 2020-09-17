257fbeebe39f324b9c3f77af3e4289de0c28cd6a
root <root@LAPTOP-96PPJ4QI.localdomain>
2020-09-17 19:13:17 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,5 @@ def number_of_subscribers(subreddit):
     h = {'user-agent': 'GEEK1050'}
     link = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     req = requests.get(link, headers=h)
-    if req.status_code is 404:
-        return 0
 
     return req.json().get("data").get("subscribers")
