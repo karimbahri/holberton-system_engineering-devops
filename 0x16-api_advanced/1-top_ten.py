3884e8623fa39165c2ca06ed79a312ab26d8c076
root <root@LAPTOP-96PPJ4QI.localdomain>
2020-09-17 19:29:43 +0100

    fix pep8

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,10 +4,12 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """return number of subscribers
+    """return top post of subscribers
        reddit"""
     h = {'user-agent': 'GEEK1050'}
     link = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     req = requests.get(link, headers=h)
 
-    return req.json().get("data").get("subscribers")
+    req_data = req.json().get("data").get("subscribers")
+    for element in req_data['children']:
+        print(element['children']['title'])
