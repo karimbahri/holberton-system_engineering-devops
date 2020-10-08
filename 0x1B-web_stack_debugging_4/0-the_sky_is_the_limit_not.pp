49cbce8b84bf756006b89daca578790030c6e865
karim bahri <karim-bahri-18@outlook.fr>
2020-10-08 23:05:18 +0100

    limit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# manifest
+exec { 'limit':
+  path    => ['/usr/bin', '/bin'],
+  command => 'sed -i \'s/^ULIMIT=.*/ULIMIT="-n 4096"/g\' /etc/default/nginx',
+}
+
+-> exec { 'reload':
+  command => 'nginx restart',
+  path    => '/etc/init.d',
+}
